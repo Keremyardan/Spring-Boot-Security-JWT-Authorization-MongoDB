ac629a8abe4ec25ac6b1b4b030d72ad848be51b5
keremyardan <keremyrdn@hotmail.com>
2024-10-19 00:41:18 +0300

    Additional classes have been created under services folder. WebSecurityConfig will be applied

--- src/main/java/com/keremyardan/nosql_auth/Entity/User.java
+++ src/main/java/com/keremyardan/nosql_auth/Entity/User.java
@@ -15,10 +15,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Document(collection = "users")
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 public class User {
     @Id
     private String id;
@@ -38,4 +34,54 @@ public class User {
 
     @DBRef
     private Set<Role> roles = new HashSet<>();
+
+    public User() {
+
+    }
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password=password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
--- src/main/java/com/keremyardan/nosql_auth/security/services/UserDetailsServiceImpl.java
+++ src/main/java/com/keremyardan/nosql_auth/security/services/UserDetailsServiceImpl.java
@@ -1,9 +1,23 @@
 package com.keremyardan.nosql_auth.security.services;
 
+import com.keremyardan.nosql_auth.Entity.User;
+import com.keremyardan.nosql_auth.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
 
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User could not found with the user name" + username));
+        return UserDetailsImpl.build(user);
+    }
 }
